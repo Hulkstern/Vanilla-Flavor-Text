112192407161001af1463e59e85e28d8b94556a4
Hulkstern <rjbare3@gmail.com>
2020-10-12 09:42:19 -0400

    Added main package & info
    Added the main package and the info file for the mod

---
+++ src/main/java/vanillaFlavorText/VanillaFlavorText.java
@@ -0,0 +1,7 @@
+package vanillaFlavorText;
+
+public class VanillaFlavorText {
+	public static final String MODID="VanillaFlavorText";
+	public static final String NAME="Vanilla Flavor Text";
+	public static final String VERSION="0.1.0";
+}
